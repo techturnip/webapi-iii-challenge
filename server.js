ed8e7a0f18bb0246125b971dcd021f74615990bd
Tyler Turnipseed <tyler@techturnip.us>
2019-08-13 17:34:20 -0400

    built out endpoints to get all users and get user by id, created custom user id validation middleware

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express')
 
 const server = express()
 
+const userRouter = require('./users/userRouter')
+
 server.use(logger)
+
+server.use('/api/users/', userRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,59 @@
-const express = 'express';
+const express = require('express')
 
-const router = express.Router();
+const router = express.Router()
 
-router.post('/', (req, res) => {
+const Users = require('./userDb')
 
-});
+router.post('/', (req, res) => {})
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', (req, res) => {})
 
-});
+router.get('/', async (req, res) => {
+  try {
+    const users = await Users.get()
+    res.status(200).json(users)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      message: 'unknown error'
+    })
+  }
+})
 
-router.get('/', (req, res) => {
+router.get('/:id', validateUserId, async (req, res) => {
+  res.status(200).json(req.user)
+})
 
-});
+router.get('/:id/posts', (req, res) => {})
 
-router.get('/:id', (req, res) => {
+router.delete('/:id', (req, res) => {})
 
-});
-
-router.get('/:id/posts', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
+router.put('/:id', (req, res) => {})
 
 //custom middleware
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
+  const { id } = req.params
 
-};
+  try {
+    const user = await Users.getById(id)
 
-function validateUser(req, res, next) {
+    if (user) {
+      req.user = user
+      next()
+    } else {
+      res.status(404).json({ message: 'No user with given id' })
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      message: 'Error processing request'
+    })
+  }
+}
 
-};
+function validateUser(req, res, next) {}
 
-function validatePost(req, res, next) {
+function validatePost(req, res, next) {}
 
-};
-
-module.exports = router;
+module.exports = router
