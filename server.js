557503d785181db2779354fbeb6a5d53a4ac8120
Tyler Turnipseed <tyler@techturnip.us>
2019-08-13 15:09:16 -0400

    created logger middleware to log the url, method and timestamp for the api request

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ const express = require('express')
 
 const server = express()
 
+server.use(logger)
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 })
 
 //custom middleware
-
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(
+    `Request to URL: '${req.url}' | Method: ${
+      req.method
+    } | Time: ${new Date().toLocaleTimeString()}`
+  )
+  next()
+}
 
 module.exports = server
