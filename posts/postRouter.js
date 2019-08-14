56ca53191cd193eb6b07fc3889ef7153e032f5f9
Tyler Turnipseed <tyler@techturnip.us>
2019-08-13 21:25:39 -0400

    added endpoint to update a post with specified id

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -46,7 +46,18 @@ router.delete('/:id', validatePostId, async (req, res) => {
   }
 })
 
-router.put('/:id', validatePostId, (req, res) => {})
+router.put('/:id', validatePostId, validatePost, async (req, res) => {
+  try {
+    const updatedPost = await Posts.update(req.post.id, req.body)
+
+    res.status(200).json(req.post)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      message: 'Could not update the specified post'
+    })
+  }
+})
 
 // CUSTOM MIDDLEWARE ------------------------------|
 // ------------------------------------------------|
@@ -70,6 +81,26 @@ function validatePostId(req, res, next) {
         message: 'Error processing request'
       })
     })
+}
+
+function validatePost(req, res, next) {
+  if (req.body && Object.keys(req.body).length > 0) {
+    const { text } = req.body
+
+    if (text) {
+      if (text.length > 2 && typeof text === 'string') {
+        next()
+      } else {
+        res
+          .status(400)
+          .json({ message: 'Text field must be a string and cannot be empty' })
+      }
+    } else {
+      res.status(400).json({ message: 'Missing required text field' })
+    }
+  } else {
+    res.status(400).json({ message: 'Missing post data' })
+  }
 }
 
 module.exports = router
